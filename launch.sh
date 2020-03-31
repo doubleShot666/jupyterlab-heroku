f65a0a36383612ae380cf441dee93fd24f3002ad
Sami Djouadi <32199144+doubleShot666@users.noreply.github.com>
2020-03-31 21:43:10 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,14 +1,24 @@
-#!/bin/bash
+#jupyter serverextension enable --py jupyterlab --sys-prefix
 
-jupyter serverextension enable --py jupyterlab --sys-prefix
+#mkdir -p root
+#cd root
 
-mkdir -p root
-cd root
-
-cp ../kernel.cpython-37m-x86_64-linux-gnu.so ./kernel.cpython-37m-x86_64-linux-gnu.so
-cp ../test_hapke.json ./test_hapke.json
-cp ../test_script.ipynb ./test_script.ipynb
+#cp ../kernel.cpython-37m-x86_64-linux-gnu.so ./kernel.cpython-37m-x86_64-linux-gnu.so
+#cp ../test_hapke.json ./test_hapke.json
+#cp ../test_script.ipynb ./test_script.ipynb
 
 
-export JUPYTER_CONFIG_DIR=/app
-jupyter lab --port=${PORT}
+#export JUPYTER_CONFIG_DIR=/app
+#jupyter lab --port=${PORT}
+
+curl -sSL https://get.docker.com/ | sh
+
+docker run -d --name gitlab-runner --restart always \
+  -v /srv/gitlab-runner/config:/etc/gitlab-runner \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  gitlab/gitlab-runner:latest
+
+docker run -d --name gitlab-runner --restart always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --volumes-from gitlab-runner-config \
+    gitlab/gitlab-runner:latest
